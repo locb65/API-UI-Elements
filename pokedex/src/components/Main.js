49847ab03256d10a8a4bce826935d6d534577286
locb65 <locb65@gmail.com>
2023-02-21 18:34:01 -0500

    static data complete, axios install

--- pokedex/src/components/Main.js
+++ pokedex/src/components/Main.js
@@ -1,27 +1,37 @@
 import React from 'react'
 import { PokemonImage } from './PokemonImage'
 import { PokemonDetails } from './PokemonDetails'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import "./Pokemon.css"
 
 export const Main = () => {
-  return (
-    <>
-    <div className='MainContainer'>
-        <div className='PokedexContainer'>
-            <PokemonImage/>
-            <PokemonImage/>
-            <PokemonImage/>
-            <PokemonImage/>
-            <PokemonImage/>
-        </div>
-        <div className='PokeDetails'>
-            <PokemonDetails/>
-            <PokemonDetails/>
-            <PokemonDetails/>
-            <PokemonDetails/>
-            <PokemonDetails/>
-        </div>
-    </div>
-    </>
-  )
-}
+    const [pokeData, setPokeData] = useState()
+
+
+    return (
+        <>
+            <div className='MainContainer'>
+                <div className='PokedexContainer'>
+                    <PokemonImage/>
+                    <PokemonImage/>
+                    <PokemonImage/>
+                    <PokemonImage/>
+                    <PokemonImage/>
+                    <div className='button-group'>
+                    <button>Previous</button>
+                    <button>Next</button>
+                </div>
+                </div>
+
+                <div className='PokeDetails'>
+                    <PokemonDetails/>
+                    <PokemonDetails/>
+                    <PokemonDetails/>
+                    <PokemonDetails/>
+                    <PokemonDetails/>
+                </div>
+            </div>
+        </>
+    )
+    }
