4025de3abaf9ab1606b95d57d2411fc0ad773cf6
Loc Bui <buda@Locs-MacBook-Pro.local>
2023-02-21 13:47:03 -0500

    got some data loaded for one pokemon

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -8,20 +8,23 @@ function App(props) {
 const [pokemon, setPokemon] = useState()
 
 function fetchPokemon() {
-  fetch(`https://pokeapi.co/api/v2/pokemon/`)
+  fetch(`https://pokeapi.co/api/v2/pokemon/${2}`)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => setPokemon(data))
 }
   useEffect(fetchPokemon, [])
 
-  
 
+console.log(pokemon)
 
 
 
   return (
-    <div className="App">
-     <Home/>
+    <div className="App" >
+      <img src={pokemon.sprites.front_default}></img>
+      {fetchPokemon}
+      <p>{pokemon.name}, height = {pokemon.height} </p>
+     <Home /> 
     </div>
   );
 }
